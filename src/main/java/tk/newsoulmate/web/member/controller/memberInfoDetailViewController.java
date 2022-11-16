9abb4524e9f0df4011be03d81538e99611806227
YEOMCODING <duatjgkr123@gmail.com>
2022-11-16 20:01:35 +0900

    yeom - 잘 되나?

--- src/main/java/tk/newsoulmate/web/member/controller/memberInfoDetailViewController.java
+++ src/main/java/tk/newsoulmate/web/member/controller/memberInfoDetailViewController.java
@@ -10,6 +10,7 @@ public class memberInfoDetailViewController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+            request.getRequestDispatcher("views/member/");
     }
 
     @Override
