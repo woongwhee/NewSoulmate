0cd4bc949cdb017c230100ff09f0078e8e3a9fc8
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-11-17 15:49:59 +0900

    첨부파일문제 해결중

--- src/main/java/tk/newsoulmate/web/adopt/controller/AdoptApplyInsertController.java
+++ src/main/java/tk/newsoulmate/web/adopt/controller/AdoptApplyInsertController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "AAdoptApplyInsertController", value = "/adoptApplyInsert")
+@WebServlet(name = "AdoptApplyInsertController", value = "/adoptApplyInsert")
 public class AdoptApplyInsertController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
