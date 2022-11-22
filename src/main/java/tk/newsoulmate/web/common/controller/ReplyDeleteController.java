52a2fec022d3b21ad33b43172998d589c6e2e4e4
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-11-22 14:29:03 +0900

    댓글삭제기능구현중

---
+++ src/main/java/tk/newsoulmate/web/common/controller/ReplyDeleteController.java
@@ -0,0 +1,51 @@
+package tk.newsoulmate.web.common.controller;
+
+import tk.newsoulmate.domain.vo.Board;
+import tk.newsoulmate.domain.vo.Member;
+import tk.newsoulmate.domain.vo.Reply;
+import tk.newsoulmate.web.adopt.sevice.AdoptService;
+import tk.newsoulmate.web.common.service.CommonService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "replyDelete", value = "/replyDelete")
+public class ReplyDeleteController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String rno_ = request.getParameter("rno");
+        HttpSession session=request.getSession();
+        if(rno_==null||rno_.equals("")){
+            error(request, response);
+            return;
+        }
+        int rno=Integer.parseInt(rno_);
+        CommonService cs=new CommonService();
+        Member loginUser = (Member) session.getAttribute("loginUser");
+        int loginMno= loginUser.getMemberNo();
+        int result=cs.deleteReply(rno,loginUser.getMemberNo());
+        if(result==0){
+            error(request,response);
+            return;
+        }
+        session.setAttribute("alertMsg","댓글삭 삭제 성공");
+        response.sendRedirect(request.getContextPath()+"/adoptReList");
+
+    }
+
+    private static void error(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().setAttribute("errorMsg","잘못된접근입니다.");
+        response.sendRedirect(request.getContextPath()+"/adoptReList");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+}
