c27a87a7e5297d8bb5e9f505021090ae84dcc7e1
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2022-12-27 03:38:36 +0900

    readme초안

--- src/main/java/tk/newsoulmate/web/common/Listener/NoticeListener.java
+++ src/main/java/tk/newsoulmate/web/common/Listener/NoticeListener.java
@@ -1,34 +1,36 @@
 package tk.newsoulmate.web.common.Listener;
 
 import tk.newsoulmate.web.common.APIKeys;
-import tk.newsoulmate.web.schedule.service.ScrapTimmer;
 
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import java.util.Timer;
 
+
 @WebListener
 public class NoticeListener implements ServletContextListener {
     private ScrapTimmer task;
-    private Timer m;
+     private Timer m;
+    final private static long oneDay=1000 * 60 * 60 * 24;
     public NoticeListener() {
+
     }
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        //
         new APIKeys();
-////        m = new Timer();
-////        task = new ScrapTimmer();
-//        long time = task.StartTimming();
-//        long oneDay = 1000 * 60 * 60 * 24;
-//        //새벽 1시마다 전날 업데이트된 데이터를 긁어오는 함수
-////        m.schedule(task,time, oneDay);
+        m = new Timer();
+        task = new ScrapTimmer();
+        long time = task.StartTimming();
+//        새벽 1시마다 전날 업데이트된 데이터를 긁어오는 함수
+        m.schedule(task,time, oneDay);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-//        task.cancel();
-//        m.cancel();
+        task.cancel();
+        m.cancel();
     }
 
 }
